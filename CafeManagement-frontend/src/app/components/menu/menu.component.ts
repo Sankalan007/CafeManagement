d9eebe132e6093e4f22b8b7c88194db0b0cde1c9
Sankalan <schanda@argusoft.com>
2023-04-18 14:56:24 +0530

    added add item form in menu section is the logged in person is admin

--- CafeManagement-frontend/src/app/components/menu/menu.component.ts
+++ CafeManagement-frontend/src/app/components/menu/menu.component.ts
@@ -1,6 +1,6 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import { MenuService } from 'src/app/services/menu/menu.service';
 
@@ -11,12 +11,28 @@ import { MenuService } from 'src/app/services/menu/menu.service';
 })
 export class MenuComponent implements OnInit {
   items!: any;
+  showFoodForm: boolean = false;
+  addItemForm!: FormGroup;
   constructor(
     private menuService: MenuService,
-    private authService: AuthService
+    private authService: AuthService,
+    private fb: FormBuilder
   ) {}
   ngOnInit(): void {
+    this.addItemForm = this.fb.group({
+      name: ['', Validators.required],
+      price: ['', Validators.required],
+      description: ['', Validators.required],
+    });
     this.getProducts();
+  }
+
+  toggleFoodForm() {
+    this.showFoodForm = !this.showFoodForm;
+  }
+
+  getRole(): boolean {
+    return this.authService.getRole();
   }
   getProducts() {
     this.menuService.getProducts().subscribe(
@@ -31,11 +47,13 @@ export class MenuComponent implements OnInit {
   }
 
   addProduct(product: any) {
+    this.showFoodForm = !this.showFoodForm;
     this.menuService.addProduct(product).subscribe(
       (res: any) => {
         this.getProducts();
       },
       (error: HttpErrorResponse) => {
+        this.getProducts();
         console.log(error.message);
       }
     );
